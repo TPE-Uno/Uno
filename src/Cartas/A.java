18653a977cb153bf60c0207782a37e5423b65153
Martín García <margarcia@itba.edu.ar>
2016-05-29 20:55:49 -0300

    Creación de interface carta

--- src/Cartas/A.java
+++ src/Cartas/A.java
@@ -1,5 +1,9 @@
 package Cartas;
 
-public class A {
-holasdsd
+public interface Carta {
+
+	public static final Integer[] NUMEROS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	public static final String[] COLORES = {"rojo", "amarillo", "azul", "verde"};
+	
+	
 }
