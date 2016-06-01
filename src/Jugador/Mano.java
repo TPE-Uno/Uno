c1afc3d8fb909eb8694ed9750206788fb20126c7
mscomazzon <mscomazzon@itba.edu.ar>
2016-06-01 11:40:05 -0300

    Agregue Equals() y hasCode() para la clase Jugador. Ademas, agregue; toString() en ambas clases, Jugador y Mano

--- src/Jugador/Mano.java
+++ src/Jugador/Mano.java
@@ -8,27 +8,37 @@ public class Mano {
 	public static final int totalCartas = 7;
 	
 	
-	public Mano (){
+	public Mano () {
 		mano = new ArrayList<Carta>(totalCartas);
 	}
 	
-	public ArrayList<Carta> getMano(){
+	public ArrayList<Carta> getMano() {
 		return (ArrayList<Carta>) this.mano;
 	}
 	
-	public void tomaCarta (Integer carta){
+	public void tomaCarta (Carta carta) {
 		this.mano.add(carta);
 	}
 	
 	
-	public void juegaCarta (Integer carta){
+	public void juegaCarta (Carta carta) {
 		this.mano.remove(carta);
 		((ArrayList<Carta>)this.mano).trimToSize();
 	}
 	
-	public void verMano(){
+	public void verMano() {
 		for(int i = 0; i<this.mano.size(); i++){
 			System.out.println(this.mano.get(i));
 		}
 	}
+	
+	@Override
+	public String toString() {
+		String string = "";
+		for(int i=0; i<this.mano.size(); i++){
+			string += this.mano.get(i).toString();
+		}
+	}
+	
+	
 }
