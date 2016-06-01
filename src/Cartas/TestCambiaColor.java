0f4f0263c081e534e5af6dbb2ed202d35964dbe6
Martín García <margarcia@itba.edu.ar>
2016-06-01 18:24:24 -0300

    Cambio en el paquete cartas

---
+++ src/Cartas/TestCambiaColor.java
@@ -0,0 +1,14 @@
+package Cartas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCambiaColor {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
