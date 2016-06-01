4f6b675020777ff9b903ba73abf9ce054f7b2363
Martín García <margarcia@itba.edu.ar>
2016-06-01 13:28:34 -0300

    Modificacion de parametros

--- src/Mazo/Mazo.java
+++ src/Mazo/Mazo.java
@@ -1,75 +1,84 @@
 package Mazo;
 
 import java.util.Collections;
+import java.util.Stack;
+
+import Cartas.*;
 
 public class Mazo {
-	
+
 	public static final int CANT_ESTANDAR = 10;
-	
+
 	private Stack<Carta> mazo;
-	
+
 	public Mazo() {
 		mazo = new Stack<Carta>();
 	}
 	public void llenarMazo() {
-		Carta c1 = new Carta (0, rojo);
-		Carta c2 = new Carta (0, verde);
-		Carta c3 = new Carta (0, amarillo);
-		Carta c4 = new Carta (0, azul);
+		Carta c1 = new CartaEstandar ("0", "rojo");
+		Carta c2 = new CartaEstandar ("0", "verde");
+		Carta c3 = new CartaEstandar ("0", "amarillo");
+		Carta c4 = new CartaEstandar ("0", "azul");
 		mazo.push(c1);
 		mazo.push(c2);
 		mazo.push(c3);
 		mazo.push(c4);
-		for(int i = 1; i < CANT_ESTANDAR; i++) {
-			Carta c1 = new Carta(i,rojo);
-			Carta c2 = new Carta(i,verde);
-			Carta c3 = new Carta(i,amarillo);
-			Carta c4 = new Carta(i,azul);
-			mazo.push(c1);
-			mazo.push(c1);
-			mazo.push(c2);
-			mazo.push(c2);
-			mazo.push(c3);
-			mazo.push(c3);
-			mazo.push(c4);
-			mazo.push(c4);
+		for(Integer i = 1; i < CANT_ESTANDAR; i++) {
+			Carta c5 = new CartaEstandar(i.toString(),"rojo");
+			Carta c6 = new CartaEstandar(i.toString(),"verde");
+			Carta c7 = new CartaEstandar(i.toString(),"amarillo");
+			Carta c8 = new CartaEstandar(i.toString(),"azul");
+			mazo.push(c5);
+			mazo.push(c5);
+			mazo.push(c6);
+			mazo.push(c6);
+			mazo.push(c7);
+			mazo.push(c7);
+			mazo.push(c8);
+			mazo.push(c8);
 		}
-		Carta c1 = new MasDos(rojo);
-		Carta c2 = new MasDos(verde);
-		Carta c3 = new MasDos(amarillo);
-		Carta c4 = new MasDos(azul);
+		Carta c9 = new MasDos("+2", "rojo");
+		Carta c10 = new MasDos("+2", "verde");
+		Carta c11 = new MasDos("+2","amarillo");
+		Carta c12 = new MasDos("+2","azul");
 		for(int i = 0; i < 2; i++) {
-			mazo.push(c1);
-			mazo.push(c2);
-			mazo.push(c3);
-			mazo.push(c4);
+			mazo.push(c9);
+			mazo.push(c10);
+			mazo.push(c11);
+			mazo.push(c12);
 		}
-		Carta c = new MasCuatro();
+		Carta c = new MasCuatro("+4","multiColor");
 		for(int i = 0; i < 4; i++) {
 			mazo.push(c);
 		}
-		Carta c1 = new Saltea(rojo);
-		Carta c2 = new Saltea(verde);
-		Carta c3 = new Saltea(amarillo);
-		Carta c4 = new Saltea(azul);
+		Carta c13 = new Saltea("saltea","rojo");
+		Carta c14 = new Saltea("saltea","verde");
+		Carta c15 = new Saltea("saltea","amarillo");
+		Carta c16 = new Saltea("saltea","azul");
 		for(int i = 0; i < 2; i++) {
-			mazo.push(c1);
-			mazo.push(c2);
-			mazo.push(c3);
-			mazo.push(c4);
+			mazo.push(c13);
+			mazo.push(c14);
+			mazo.push(c15);
+			mazo.push(c16);
 		}
-		Carta c1 = new MultiColor(rojo);
-		Carta c2 = new MultiColor(verde);
-		Carta c3 = new MultiColor(amarillo);
-		Carta c4 = new MultiColor(azul);
+
+		Carta c17 = new CambiaColor("multiColor","no-color");
+		for(int i = 0; i < 4; i++) {
+			mazo.push(c17);
+
+		}
+
+		Carta c18 = new CambiaSentido("cambiaSentido","rojo");
+		Carta c19 = new CambiaSentido("cambiaSentido","verde");
+		Carta c20 = new CambiaSentido("cambiaSentido","amarillo");
+		Carta c21 = new CambiaSentido("cambiaSentido","azul");
 		for(int i = 0; i < 2; i++) {
-			mazo.push(c1);
-			mazo.push(c2);
-			mazo.push(c3);
-			mazo.push(c4);
+			mazo.push(c18);
+			mazo.push(c19);
+			mazo.push(c20);
+			mazo.push(c21);
 		}
-	}
-	
+	}	
 	public void mezclar() {
 		Collections.shuffle(mazo);
 	}
