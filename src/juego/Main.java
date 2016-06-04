20b7bbb4305c54162aea1a97c84c03dcff9e4f4c
nicolekapp <kappnicole.nk@gmail.com>
2016-06-04 15:17:42 -0300

    Cambios

--- src/juego/Main.java
+++ src/juego/Main.java
@@ -1,5 +1,42 @@
 package juego;
 
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+import cartas.Carta;
+import jugador.Jugador;
+import mazo.Mazo;
+
 public class Main {
 
+	private Mazo mazo = new Mazo();
+	private Mazo pozo = new Mazo();
+	private List <Jugador> l = new LinkedList<Jugador>();
+	private int cantJugadores;
+	private Carta c;
+	private String color;
+	Random rand;
+
+
+	public void iniciarJuego( int cantJugadores, ArrayList<String> nombres  ) {
+		this.cantJugadores = cantJugadores;
+		for ( String j : nombres){
+			Jugador j1 = new Jugador(j);
+			l.add(j1);
+		}
+		Juego juego = new Juego();
+		juego.setActual(l.get((rand.nextInt(cantJugadores - 1) +1)));
+		mazo.llenarMazo();
+		mazo.mezclar();
+		c = mazo.pop();
+		pozo.push(c);
+		color = c.getColor();
+		
+	}
+	
+	
 }
+
