c63241f57f544f26583f135d9c0383cc716514a3
nicolekapp <kappnicole.nk@gmail.com>
2016-06-04 16:25:50 -0300

    Cambios en Main

--- src/juego/Main.java
+++ src/juego/Main.java
@@ -14,10 +14,31 @@ public class Main {
 	private Mazo mazo = new Mazo();
 	private Mazo pozo = new Mazo();
 	private List <Jugador> l = new LinkedList<Jugador>();
-	private int cantJugadores;
+	private Integer cantJugadores;
 	private Carta c;
 	private String color;
 	Random rand;
+
+
+	public static void main(String[] args) {
+		Main inicio = new Main();
+		Menu menu = new Menu();
+
+		menu.opciones();
+
+		switch (menu.getOpcion()){
+			case 1: 			
+				inicio.iniciarJuego(menu.getCantJugadores(), menu.getJugadores());
+				break;
+			case 2: //reanudar partida
+				break;
+			case 3: //salir
+				break;
+			case 4: //instrucciones
+				break;
+			
+		}
+	}
 
 
 	public void iniciarJuego( int cantJugadores, ArrayList<String> nombres  ) {
@@ -33,9 +54,16 @@ public class Main {
 		c = mazo.pop();
 		pozo.push(c);
 		color = c.getColor();
-		
+		repartir();
+
 	}
 	
-	
+	public void repartir(){
+		for( Jugador j : l){
+			j.tomaCartas(mazo.pop(),7);
+		}
+	}
+
+
 }
 
