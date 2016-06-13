1d273119e7e75e6182aac81fadd6f1103f9746ee
Martín García <margarcia@itba.edu.ar>
2016-06-13 02:24:19 -0300

    Update RunnerDelJuego.java

--- src/controlador/RunnerDelJuego.java
+++ src/controlador/RunnerDelJuego.java
@@ -3,7 +3,7 @@ package controlador;
 public class RunnerDelJuego {
 
 	public static void main(String[] args) {
-		ControladorDelJuego juego = new ControladorDelJuego();
-		juego.inicio();
+		ControladorDelJuego juegoControl = new ControladorDelJuego();
+		juegoControl.abreVentana();
 	}
 }
