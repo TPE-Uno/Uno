ffc6ea91428e563f1e082970d7842a6f7d3fe704
Martín García <margarcia@itba.edu.ar>
2016-06-13 02:15:36 -0300

    Update CambiaColor.java

--- src/cartas/CambiaColor.java
+++ src/cartas/CambiaColor.java
@@ -7,12 +7,17 @@ package cartas;
  */
 public class CambiaColor extends CartaEspecial {
 
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 		private String colorElegido;
 	/**
 	 * M�todo constructor que crea una carta Cambia color
 	 */
 	public CambiaColor() {
 		super("cambiaColor", "NULL");
+		this.colorElegido = "";
 	}
 
 	public String getColorElegido() {
