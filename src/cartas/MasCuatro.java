0ebcc176c3199ff883c85481b267564ff47fd9c4
Martín García <margarcia@itba.edu.ar>
2016-06-13 02:19:16 -0300

    Update MasCuatro.java

--- src/cartas/MasCuatro.java
+++ src/cartas/MasCuatro.java
@@ -5,12 +5,42 @@ package cartas;
  * @author Team - UNO
  *
  */
-public class MasCuatro extends CartaEspecial {
-	
+public class CartaEstandar extends Carta {
+
 	/**
-	 * M�todo constructor que crea la carta +4
+	 * 
 	 */
-	public MasCuatro() {
-		super("+4", "NULL");
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Método constructor que crea una carta estándar
+	 * 
+	 * @param numero de la carta
+	 * @param color de la carta
+	 */
+	public CartaEstandar(final  String numero, final String color) {
+		super(numero, color);
+		if(!validaNumero(numero)) {
+			throw new IllegalArgumentException("No es un numero valido");
+		}
+		if(!validaColor(color)) {
+			throw new IllegalArgumentException("No es un color valido");
+		}
+	}	
+
+	/**
+	 * Método que valida si el número que se pasa por parametro está dentro
+	 * de los números permitidos por el juego
+	 *
+	 * @param numero
+	 * @return "true" si el número es válido y "false" si no lo es
+	 */
+	private static boolean validaNumero(String numero) {
+		for(String valor : VALORES) {
+			if(valor.equals(numero)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
