8220af5755ee84b1d26b256c3c6879d65ec801e8
Martín García <margarcia@itba.edu.ar>
2016-06-16 01:55:02 -0300

    Update JuegoFrame.java

--- src/vista/JuegoFrame.java
+++ src/vista/JuegoFrame.java
@@ -1,25 +1,34 @@
 package vista;
 
+import java.awt.Image;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 import controlador.ControladorDelJuego;
 
+@SuppressWarnings("serial")
 public class JuegoFrame extends JFrame {
 
 	private MenuPanel menu;
-	private InstruccionesPanel instrucciones;
 	
-	public JuegoFrame(ControladorDelJuego juego) {
+	public JuegoFrame(ControladorDelJuego juegoControl) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setResizable(false);
-		menu = new MenuPanel(juego);
+		menu = new MenuPanel(juegoControl);
 		setupFrame(menu);
 		this.setLocationRelativeTo(null);
+		Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/imagenes/icon.png"));
+        setIconImage(icon);
+        setVisible(true);
 	}
 	
 	public void setupFrame(JPanel panel) {
 		this.setContentPane(panel);
-		this.setSize(600, 450);
+		this.setSize(1240, 600);
 		this.setVisible(true);
+	}
+	
+	public MenuPanel getMenu() {
+		return menu;
 	}
 }
