11da86c7b4839d0e582e79e1c21a584b45814baa
Martín García <margarcia@itba.edu.ar>
2016-06-16 01:34:16 -0300

    Create CartaTest.java

---
+++ src/cartas/CartaTest.java
@@ -0,0 +1,21 @@
+package cartas;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CartaTest {
+
+	@Test
+	public void testComparaColor() {
+		
+		assertTrue(Carta.validaColor("azul"));
+		assertTrue(Carta.validaColor("verde"));
+		assertTrue(Carta.validaColor("amarillo"));
+		assertTrue(Carta.validaColor("rojo"));
+		assertFalse(Carta.validaColor("perro"));
+		assertFalse(Carta.validaColor("1000000000000"));
+		assertFalse(Carta.validaColor(null));
+		assertFalse(Carta.validaColor("$"));
+	
+	}
+}
