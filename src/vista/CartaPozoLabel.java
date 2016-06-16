fa7d320d251fd432c2d38faebcc1ac7ddcead6ef
Martín García <margarcia@itba.edu.ar>
2016-06-16 02:04:32 -0300

    Update CartaPozoLabel.java

--- src/vista/CartaPozoLabel.java
+++ src/vista/CartaPozoLabel.java
@@ -1,44 +1,12 @@
 package vista;
 
-import java.awt.Dimension;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import controlador.ControladorDelJuego;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 @SuppressWarnings("serial")
-public class CartaMazoLabel extends JLabel {
+public class CartaPozoLabel extends CartaLabel {
 
-	private boolean active;
-	
-	public CartaMazoLabel(ControladorDelJuego juegoControl, PartidaPanel partida) {
-		this.active = true;
-		setIcon(new ImageIcon(PartidaPanel.class.getResource("/imagenes/TraseraUNO.jpg")));
-		setBounds(568, 180, 90, 65);
-		setSize(new Dimension(54, 62));
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(active) {
-					juegoControl.darCartaAJugador();
-					if(juegoControl.faltanCartas()) {
-						FaltaDeCartasPanel frame = new FaltaDeCartasPanel();
-						juegoControl.getJuegoFrame().setupFrame(frame);	
-					}
-					partida.remove(partida.getJugadorActualPanel());
-					JugadorPanel nuevoJugadorActual = new JugadorPanel(juegoControl, partida);
-					partida.setJugadorActuaLabel(nuevoJugadorActual);
-					partida.add(partida.getJugadorActualPanel());
-					partida.cartaMazoOff();
-					partida.pasoOn();
-					partida.repaint();
-				}			
-			}
-		});
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
+	public CartaPozoLabel(String valor, String color, ControladorDelJuego juegoControl, PartidaPanel partida) {
+		super(valor, color, juegoControl, partida);
+		setBounds(620, 180, 49, 62);
 	}
 }
