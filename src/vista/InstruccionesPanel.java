73efc5d2d92c7efc42589103c9f5f0616e06b1dc
Martín García <margarcia@itba.edu.ar>
2016-06-16 01:54:33 -0300

    Update InstruccionesPanel.java

--- src/vista/InstruccionesPanel.java
+++ src/vista/InstruccionesPanel.java
@@ -2,16 +2,31 @@ package vista;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import controlador.ControladorDelJuego;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
+@SuppressWarnings("serial")
 public class InstruccionesPanel extends JPanel {
-
-	/**
-	 * Create the panel.
-	 */
-	public InstruccionesPanel() {
+	
+	public InstruccionesPanel(ControladorDelJuego juegoControl) {
 		setLayout(null);
-		JLabel lblHola = new JLabel("Soy las instrucciones");
-		lblHola.setBounds(167, 53, 144, 161);
-		add(lblHola);
+		JButton btnAtras = new JButton("Atras");
+		btnAtras.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				juegoControl.getJuegoFrame().setupFrame(juegoControl.getJuegoFrame().getMenu());
+			}
+		});
+		btnAtras.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnAtras.setBounds(25, 530, 157, 28);
+		add(btnAtras);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(InstruccionesPanel.class.getResource("/imagenes/Instrucciones.jpg")));
+		lblNewLabel.setBounds(0, 0, 1240, 600);
+		add(lblNewLabel);
 	}
 }
